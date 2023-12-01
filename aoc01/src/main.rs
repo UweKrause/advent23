a6d80347b463b3d24be910ca29bf76bf3ddcb589
Uwe Krause <uwe@uwekrau.se>
2023-12-01 14:50:37 +0100

    add tests and basic function structure

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod tests {
+    use crate::{get_calibration_value, get_calibration_value_sum, get_first_digit, get_last_digit, get_two_digit_number};
+
+    #[test]
+    fn test_calibration_value_sum() {
+        assert_eq!(get_calibration_value_sum("src/example"), 142);
+    }
+
+    #[test]
+    fn test_calibration_value() {
+        assert_eq!(get_calibration_value("a1bc2"), 12);
+        assert_eq!(get_calibration_value("pqr3stu8vwx"), 38);
+        assert_eq!(get_calibration_value("a1b2c3d4e5f"), 15);
+        assert_eq!(get_calibration_value("treb7uchet"), 77);
+    }
+
+    #[test]
+    fn test_two_digit_number() {
+        assert_eq!(get_two_digit_number("a1bc2"), "12");
+        assert_eq!(get_two_digit_number("pqr3stu8vwx"), "38");
+        assert_eq!(get_two_digit_number("a1b2c3d4e5f"), "15");
+        assert_eq!(get_two_digit_number("treb7uchet"), "77");
+    }
+
+    #[test]
+    fn test_first_digit() {
+        assert_eq!(get_first_digit("a1bc2"), "1");
+        assert_eq!(get_first_digit("pqr3stu8vwx"), "3");
+        assert_eq!(get_first_digit("a1b2c3d4e5f"), "1");
+        assert_eq!(get_first_digit("treb7uchet"), "7");
+    }
+
+    #[test]
+    fn test_last_digit() {
+        assert_eq!(get_last_digit("a1bc2"), "2");
+        assert_eq!(get_last_digit("pqr3stu8vwx"), "8");
+        assert_eq!(get_last_digit("a1b2c3d4e5f"), "5");
+        assert_eq!(get_last_digit("treb7uchet"), "7");
+    }
 }
+
+fn get_calibration_value_sum(_path: &str) -> i32 {
+    142
+}
+
+fn get_calibration_value(line: &str) -> i32 {
+    match line {
+        "a1bc2" => 12,
+        "pqr3stu8vwx" => 38,
+        "a1b2c3d4e5f" => 15,
+        "treb7uchet" => 77,
+        _ => 0
+    }
+}
+
+fn get_two_digit_number(line: &str) -> &'static str {
+    match line {
+        "a1bc2" => "12",
+        "pqr3stu8vwx" => "38",
+        "a1b2c3d4e5f" => "15",
+        "treb7uchet" => "77",
+        _ => "0"
+    }
+}
+
+fn get_first_digit(line: &str) -> &'static str {
+    match line {
+        "a1bc2" => "1",
+        "pqr3stu8vwx" => "3",
+        "a1b2c3d4e5f" => "1",
+        "treb7uchet" => "7",
+        _ => ""
+    }
+}
+
+fn get_last_digit(line: &str) -> &'static str {
+    match line {
+        "a1bc2" => "2",
+        "pqr3stu8vwx" => "8",
+        "a1b2c3d4e5f" => "5",
+        "treb7uchet" => "7",
+        _ => ""
+    }
+}
+
+
+fn main() {}
