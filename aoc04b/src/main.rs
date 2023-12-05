080a5949c4b977d6da4f135ca4d95a6e4d742dee
Uwe Krause <uwe@uwekrau.se>
2023-12-05 17:44:58 +0100

    implement part 2

--- aoc04b/src/main.rs
+++ aoc04b/src/main.rs
@@ -1,46 +1,49 @@
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 use std::fs::read_to_string;
 
-
 struct Card {
-    _id: u32,
-    numbers_winning: Numbers,
-    numbers_you_have: Numbers,
+    id: u32,
+    worth: u32,
 }
 
 impl Card {
     fn from(s: &str) -> Self {
+
+        //       vv
         // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        //      ^  ^^^^^^^^^^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^
-        //      |  |                Numbers you have
-        //      |  Winning Numbers
-        //      Card ID
+        // ^^^^^^  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        // |       split_numbers
+        // split_game
+        let (split_game, split_numbers) = s.split_once(": ").unwrap();
 
-        let mut split = s.split(": ");
-        let id: u32 = split.next().unwrap()
-            .split_whitespace().last().unwrap()
-            .parse().unwrap();
+        //     v
+        // Card 1:
+        // ^^^^ ^
+        // _    split_game_id
+        let (_, split_game_id) = split_game.split_once(" ").unwrap();
 
-        let mut split_numbers = split.next().unwrap().split(" | ");
-        let numbers_winning = Numbers::from(split_numbers.next().unwrap());
-        let numbers_you_have = Numbers::from(split_numbers.next().unwrap());
+        //               vvv
+        // 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        // ^^^^^^^^^^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^
+        // |                split_numbers_you_have
+        // split_numbers_winning
+        let (split_numbers_winning, split_numbers_you_have) = split_numbers.split_once(" | ").unwrap();
 
-        Card { _id: id,  numbers_winning, numbers_you_have }
+        // "1" -> 1
+        let id: u32 = split_game_id.trim().parse().unwrap();
+
+        let numbers_winning = Numbers::from(split_numbers_winning);
+        let numbers_you_have = Numbers::from(split_numbers_you_have);
+
+        let worth = Self::worth(numbers_winning, numbers_you_have);
+
+        Self { id, worth }
     }
 
-    fn numbers_matching(&self) -> HashSet<u32> {
-        self.numbers_winning.numbers
-            .intersection(&self.numbers_you_have.numbers)
-            .map(|i| i.clone())
-            .collect()
-    }
-
-    fn worth(&self) -> usize {
-        match self.numbers_matching().len() {
-            0 => 0,
-            1 => 1,
-            x => 2_usize.pow((x - 1) as u32)
-        }
+    fn worth(numbers_winning: Numbers, numbers_you_have: Numbers) -> u32 {
+        numbers_winning.numbers
+            .intersection(&numbers_you_have.numbers)
+            .count() as u32
     }
 }
 
@@ -51,6 +54,10 @@ struct Numbers {
 
 impl Numbers {
     fn from(s: &str) -> Self {
+        //   v  vv v  v  vv v  v
+        // 83 86  6 31 17  9 48 53
+        // ^^ ^^  ^ ^^ ^^  ^ ^^ ^^
+        // numbers
         let numbers = s.split_whitespace()
             .map(|s| s.parse().unwrap())
             .collect();
@@ -60,10 +67,37 @@ impl Numbers {
 
 
 fn main() {
-    let worth_sum: usize = read_to_string("src/input").unwrap().lines()
-        .map(|line| Card::from(line))
-        .map(|c| c.worth())
-        .sum();
 
-    println!("{}", worth_sum); // 26346
+    // ids in input start at 1.
+    // in an attempt to minimize the confusion,
+    // lets also do this
+
+    let mut pile: BTreeMap<u32, u32> = BTreeMap::new();
+
+    // For every card
+    // Add the original card to the pile of cards
+    // pile[current.id] += 1
+    //
+    // Add one for as much tickets as the current ticket is worth:
+    // for current.id+1..current.id+1+current.worth
+    //   pile[current.id+1] += current.worth
+    for card in read_to_string("src/input").unwrap().lines().into_iter() {
+        let card = Card::from(card);
+
+        let id = card.id;
+
+        // ToDo: I got neither .entry() nor DefaultBtreeMap working...
+        let current = pile.get(&id).unwrap_or(&0);
+        pile.insert(id, current + 1);
+
+        let current = pile.get(&id).unwrap_or(&0).to_owned();
+        let from = card.id + 1;
+        let to = from + card.worth;
+        for next_nth in from..to {
+            pile.insert(next_nth, pile.get(&next_nth).unwrap_or(&0) + current);
+        }
+    }
+
+    let card_sum: u32 = pile.values().sum();
+    println!("{}", card_sum)
 }
